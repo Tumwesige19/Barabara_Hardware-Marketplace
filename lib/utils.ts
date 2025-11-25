6639b6d1e69eb33fbf73f09968ec43ec792b600b
Tumwesige19 <tumwesige19@example.com>
2025-11-24 18:43:25 -0600

    Add cn utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,7 @@
 export function formatCurrency(amount: number): string {
     return `UGX ${amount.toLocaleString()}`;
 }
+
+export function cn(...classes: (string | undefined | null | false)[]) {
+    return classes.filter(Boolean).join(" ");
+}
